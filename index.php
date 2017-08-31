f4a39991de67bb52dbff78815e0393997966c70f
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2017-08-31 22:19:29 +0700

    || :)

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php 
 //mysql://bf60e5c13b1643:ffdf89ba@us-cdbr-iron-east-05.cleardb.net/heroku_733e7485a90f5eb?reconnect=trues
-echo "hello COJ CD 101 :)";
-$CLEARDB_DATABASE_URL = "mysql://bf60e5c13b1643:ffdf89ba@us-cdbr-iron-east-05.cleardb.net/heroku_733e7485a90f5eb?reconnect=true";
-$url = parse_url(getenv($CLEARDB_DATABASE_URL));
+echo "|| hello COJ CD 101 :)";
+//$CLEARDB_DATABASE_URL = "mysql://bf60e5c13b1643:ffdf89ba@us-cdbr-iron-east-05.cleardb.net/heroku_733e7485a90f5eb?reconnect=true";
+//$url = parse_url(getenv($CLEARDB_DATABASE_URL));
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
 $username = $url["user"];
