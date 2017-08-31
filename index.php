a4e5e623e8d4683aee6d51bdef599ca42d580f7c
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2017-08-31 22:15:39 +0700

    test database

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php 
 //mysql://bf60e5c13b1643:ffdf89ba@us-cdbr-iron-east-05.cleardb.net/heroku_733e7485a90f5eb?reconnect=trues
 echo "hello COJ CD 101";
-
-$url = parse_url(getenv("us-cdbr-iron-east-05.cleardb.net"));
+$CLEARDB_DATABASE_URL = "mysql://bf60e5c13b1643:ffdf89ba@us-cdbr-iron-east-05.cleardb.net/heroku_733e7485a90f5eb?reconnect=true";
+$url = parse_url(getenv($CLEARDB_DATABASE_URL));
 
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
-
+print_r($url);
 $conn = new mysqli($server, $username, $password, $db);
 $result = $conn->query("SELECT * FROM tb_users");
 echo $result->num_rows;
