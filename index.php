6a1c012331352a1743b6efde425e98e55fbf7312
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2017-09-01 20:30:24 +0700

    test dump db

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
-echo "^^|| hello COJ CD 101 :)";
+echo "Test clone db data...";
 echo "<hr>";
 
 /*
 // true = local host, false = deploy on server
 */
-$debug = true; 
+$debug = false; 
+
 
 //$CLEARDB_DATABASE_URL = "mysql://bf60e5c13b1643:ffdf89ba@us-cdbr-iron-east-05.cleardb.net/heroku_733e7485a90f5eb?reconnect=true";
 //$url = parse_url(getenv($CLEARDB_DATABASE_URL));
-$url_var = "mysql://lbkcadmin:Lbkc@db#2016@localhost/heroku_733e7485a90f5eb";
+$url_var = "mysql://cojcd_admin:nffuF5S5Y9GUP5yw@localhost/heroku_733e7485a90f5eb";
 if(!$debug){
     //$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $url_var = getenv("CLEARDB_DATABASE_URL");
@@ -31,23 +32,6 @@ if($conn->get_connection_stats()){
     exit;
 }
 echo "<hr>";
-
-for($i=0;$i<100;$i++){
-    $id_card = "10901299088769"+$i;
-    $email   = "demo$i@mail.com";
-    $username = "demo$i";
-    $password = md5("demo0075"+$i);
-    $result = $conn->query("INSERT INTO tb_users (id_card,email,username,password,register_date) 
-    VALUES ('$id_card','$email','$username','$password',NOW())");
-    echo $conn->insert_id." >> ";
-    echo $result ? "เพิ่มข้อมูลได้สำเร็จ":"ไม่สามารถเพิ่มข้อมูลได้";
-    echo "<hr>";
-
-    if(!$result){
-        break;
-    }
-
-}
 
 $sql0 = "SELECT * FROM tb_users";
 $result2 = $conn->query($sql0);
