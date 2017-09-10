96043c0b264126e4449a7df83d9aa313958354f4
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2017-09-10 21:07:35 +0700

    test on host # 4 fix main page

--- main.php
+++ main.php
@@ -19,22 +19,21 @@
     // array("title"=>"link")
     $PAGE_BREAD_CRUMB = array();
     
-    print_r($_SESSION);
+    // print_r($_SESSION);
 ?>
 
-<?php //include_once "include/header.php"; ?>
-<?php //include_once "include/header_end.php"; ?>
+<?php include_once "include/header.php"; ?>
+<?php include_once "include/header_end.php"; ?>
 
 <!-- Content -->
-HELLO WORLD #3
 <?php
-    //include_once "main_m1.php";     // display main page of normal member
-    //include_once "main_m2.php";     // display main page of resident member
-    //include_once "main_admin.php";  // display main page of admin
+    include_once "main_m1.php";     // display main page of normal member
+    include_once "main_m2.php";     // display main page of resident member
+    include_once "main_admin.php";  // display main page of admin
 ?>
 
 </div>
 <!-- End Content (end container div) -->
 
-<?php //include_once "include/footer.php"; ?>
-<?php //include_once "include/footer_end.php"; ?>
+<?php include_once "include/footer.php"; ?>
+<?php include_once "include/footer_end.php"; ?>
