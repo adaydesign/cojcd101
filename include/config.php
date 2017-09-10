aa579a1922cdda3c43a9b076d858d680f797b0f5
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2017-09-10 21:07:59 +0700

    test on host # 4.1 fix main page

--- include/config.php
+++ include/config.php
@@ -8,12 +8,13 @@ define("SYS_VERSION","0.5.3");
 //-------------------- Database connection -------------------------
 define("DB_LOCAL_TEST",false);
 define("DB_OFFLINE_URL","mysql://cojcd_admin:nffuF5S5Y9GUP5yw@localhost/heroku_733e7485a90f5eb");
-//"mysql://bf60e5c13b1643:ffdf89ba@us-cdbr-iron-east-05.cleardb.net/heroku_733e7485a90f5eb"
 define("DB_ONLINE_URL","CLEARDB_DATABASE_URL");
+//define("DB_ONLINE_URL","mysql://bf60e5c13b1643:ffdf89ba@us-cdbr-iron-east-05.cleardb.net/heroku_733e7485a90f5eb");
 
 $url_var = DB_OFFLINE_URL;
 if(!DB_LOCAL_TEST){
     $url_var = getenv(DB_ONLINE_URL);
+    //$url_var = DB_ONLINE_URL;
 }
 $url        = parse_url($url_var);
 $server     = $url["host"];
