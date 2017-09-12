ce1434aecedd6d40612957d2ce7955339f0c4d1e
comseven <comseven@coj.go.th>
2017-09-12 15:57:01 +0700

    v.0.7.1

--- include/config.php
+++ include/config.php
@@ -5,7 +5,7 @@ include_once "db_connection.config.php";
 //-------------------- System Version ------------------------------
 define("SYS_NAME","ระบบบริหารจัดการห้องพักศาลยุติธรรม");
 define("SYS_NAME_2","CCMS");
-define("SYS_VERSION","0.6.5");
+define("SYS_VERSION","0.7.1");
 
 //-------------------- PAGE TOP MENU CONSTANT ------------------------------------
 define("TOPMENU_INDEX_HOME",0);
