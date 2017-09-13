d13f50d61a13fc01171a0ecc0214a11519c054fc
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2017-09-14 01:21:02 +0700

    :) bot

--- bot.php
+++ bot.php
@@ -5,12 +5,6 @@ include_once "vendor/autoload.php";
 $accessToken   = "4CaPW0nMt7/g/JJlwLv5Na3FrXaKlXT8vafjvcTbKLrBoRGTYb+nCLZvn8uHvDNlw+UbXaBBwGC0Sm5smUw34B6S0ezNNM/ztd5knT+tQOeLRv4k4Oz/ZiE1j1jslrhCjpidRnjoW1TfIG1gb2MymQdB04t89/1O/w1cDnyilFU=";
 $channelSecret = "9e65a3cdf681cfff0abaaa0cdb0edd1f";
 
-//$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
-//$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
-//$response = $bot->replyText('U2da8b19feaedf50a33baaeb80ce64ba', 'hello!');
-
-
 include_once "line-bot.php";
 
 $bot = new BOT_API($channelSecret, $accessToken);
@@ -20,8 +14,14 @@ if (!empty($bot->isEvents)) {
     $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
     if ($bot->isSuccess()) {
         echo 'Succeeded!';
+        //exit();
+    }
+    $bot->sendMessageNew('U2da8b19feaedf50a33baaeb80ce64ba2', 'Hello World !!');
+    if ($bot->isSuccess()) {
+        echo 'Succeeded!';
         exit();
     }
+
     // Failed
     echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
     exit();
