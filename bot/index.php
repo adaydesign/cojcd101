cac42a232bad90364d3b7bf95ab90b894896ae47
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2017-09-14 00:12:34 +0700

    :)

--- bot/index.php
+++ bot/index.php
@@ -1,12 +1,6 @@
 <?
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('4CaPW0nMt7/g/JJlwLv5Na3FrXaKlXT8vafjvcTbKLrBoRGTYb+nCLZvn8uHvDNlw+UbXaBBwGC0Sm5smUw34B6S0ezNNM/ztd5knT+tQOeLRv4k4Oz/ZiE1j1jslrhCjpidRnjoW1TfIG1gb2MymQdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '9e65a3cdf681cfff0abaaa0cdb0edd1f']);
-
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('U2da8b19feaedf50a33baaeb80ce64ba2', $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+echo "hello bot ??";
 
 
 ?>
