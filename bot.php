5b68dd37815a7558fb382a25202ece83e167887d
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2017-09-14 01:02:59 +0700

    :)

--- bot.php
+++ bot.php
@@ -2,9 +2,29 @@
 
 include_once "vendor/autoload.php";
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('4CaPW0nMt7/g/JJlwLv5Na3FrXaKlXT8vafjvcTbKLrBoRGTYb+nCLZvn8uHvDNlw+UbXaBBwGC0Sm5smUw34B6S0ezNNM/ztd5knT+tQOeLRv4k4Oz/ZiE1j1jslrhCjpidRnjoW1TfIG1gb2MymQdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '9e65a3cdf681cfff0abaaa0cdb0edd1f']);
+$accessToken   = "4CaPW0nMt7/g/JJlwLv5Na3FrXaKlXT8vafjvcTbKLrBoRGTYb+nCLZvn8uHvDNlw+UbXaBBwGC0Sm5smUw34B6S0ezNNM/ztd5knT+tQOeLRv4k4Oz/ZiE1j1jslrhCjpidRnjoW1TfIG1gb2MymQdB04t89/1O/w1cDnyilFU=";
+$channelSecret = "9e65a3cdf681cfff0abaaa0cdb0edd1f";
 
-$response = $bot->replyText('U2da8b19feaedf50a33baaeb80ce64ba', 'hello!');
+//$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
+//$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+//$response = $bot->replyText('U2da8b19feaedf50a33baaeb80ce64ba', 'hello!');
+
+
+include_once "line-bot.php";
+
+$bot = new BOT_API($channelSecret, $accessToken);
+	
+if (!empty($bot->isEvents)) {
+		
+    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+    if ($bot->isSuccess()) {
+        echo 'Succeeded!';
+        exit();
+    }
+    // Failed
+    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+    exit();
+}
 
 ?>
